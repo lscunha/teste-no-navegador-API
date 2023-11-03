9788c8ec4b6b45ff095bb1d3646ad95d90f7b9fd
Lucas Santos <lucas.lsc@hotmail.com.br>
2023-11-02 23:31:40 -0300

    first commit

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-## 04c - Atividade 3 - Criar estrutura da API com Context e método GET para projetos, teste no navegador
-
+## 04d - Atividade 4 - Criar o restante do CRUD de projetos, testando-o no Insomnia
 # Descrição:
-
-Para esta atividade será utilizado o Editor de código Visual Studio Code e o SDK do .NET nas versões 6 ou 7.
+Para esta atividade será utilizado o Editor de código Visual Studio Code e o SDK do .NET nas versões 6 ou 7, que pode ser obtido em: https://code.visualstudio.com.
 
 A aplicação necessitará também da criação de um banco de dados (anexo “cria-db.sql”) no SSMS (SQL Server Management Studio).
+Será disponibilizado um projeto (anexo “MATERIAL-PARA-O-ALUNO-FAZER-ATIVIDADE”) de API pré construído para você implementar o código necessário completar o restante do CRUD da aplicação.
 
-Será disponibilizado um projeto (anexo “MATERIAL-PARA-O-ALUNO-FAZER-ATIVIDADE) de API pré construído para você implementar o código necessário para a listagem dos projetos cadastrados no banco de dados.
-
-Após o desenvolvimento do método listar, desenvolver as demais implementações, os testes serão efetuados no navegador. 
+Após o desenvolvimento do restante do CRUD, os testes serão efetuados no aplicativo Insomnia (ou Postman).  
--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -1,14 +1,48 @@
+using Exo.WebApi.Contexts;
+using Exo.WebApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-namespace Exo.WebApi.Models
+namespace Exo.WebApi.Repositories
 {
-    public class Projeto
+    public class ProjetoRepository
     {
-        public int Id { get; set; }
-        public string NomeDoProjeto { get; set; }
-        public string Area { get; set; }
-        public bool Status { get; set; }
-    }
+        private readonly ExoContext _context;
+        public ProjetoRepository(ExoContext context)
+        {
+            _context = context;
+        }
+        public List<Projeto> Listar()
+        {
+            return _context.Projetos.ToList();
+        }
+        // Código novo que completa o CRUD.
+        public void Cadastrar(Projeto projeto)
+        {
+            _context.Projetos.Add(projeto);
+            _context.SaveChanges();
+        }
+        public Projeto BuscarporId(int id)
+        {
+            return _context.Projetos.Find(id);
+        }
+{
+Projeto projetoBuscado = _context.Projetos.Find(id);
+if (projetoBuscado != null)
+{
+projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+projetoBuscado.Area = projeto.Area;
+projetoBuscado.Status = projeto.Status;
+}
+    _context.Projetos.Update(projetoBuscado);
+_context.SaveChanges();
+}
+public void Deletar(int id)
+{
+    Projeto projetoBuscado = _context.Projetos.Find(id);
+    _context.Projetos.Remove(projetoBuscado);
+    _context.SaveChanges();
+}
+}
 }
